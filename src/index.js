1e69400e0d8f02d9c216e61f0b86750a141859bc
Ludmila Mychta <ljuda12@gmail.com>
2020-11-29 23:58:22 +0300

    создала рисунок

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+/*
+=========  v1. создает линку на цензор   =========  
+
 const link = React.createElement(
   'a',
   {
@@ -13,3 +16,23 @@ const link = React.createElement(
 console.log("создаю линку", link);
 
 ReactDOM.render(link, document.getElementById('root'));
+*/
+
+
+const image = React.createElement('img', {
+  src:
+    'https://images.pexels.com/photos/461198/pexels-photo-461198.jpeg?dpr=2&h=480&w=640',
+  alt: 'Tacos With Lime',
+  width: 640,
+});
+const title = React.createElement('h2', null, 'Tacos With Lime');
+const price = React.createElement('p', null, 'Price: 10.99$');
+const button = React.createElement('button', { type: 'button' }, 'Add to cart');
+
+const product = React.createElement('div', null, image, title, price, button);
+
+const productWithChildrenInProps = React.createElement('div', {
+  children: [image, title, price, button],
+});
+
+ReactDOM.render(image, document.getElementById('root'));
