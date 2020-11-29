ccbb5ae8a917fdfb9d70f334122e5a38a4b62778
ludmiladev <ljuda12@gmail.com>
2020-11-30 01:20:04 +0200

    JavaScript Syntax Extension (JSX) перепишем карточку продукта

--- src/index.js
+++ src/index.js
@@ -53,10 +53,28 @@ ReactDOM.render(image, document.getElementById('root'));  */
 console.log("создаю линку", link);
 
 ReactDOM.render(link, document.getElementById('root')); */
-    
+
+/*
 const linkWithJSX = (
   <a href="https://reactjs.org/" target="_blank" rel="noreferrer noopener">
     Ссылка на reactjs.org
   </a>
 );
-ReactDOM.render(linkWithJSX, document.getElementById('root'));
+ReactDOM.render(linkWithJSX, document.getElementById('root'));*/
+
+// =================================V5 JavaScript Syntax Extension (JSX) перепишем карточку продукта======================
+
+const imageUrl =
+  'https://images.pexels.com/photos/461198/pexels-photo-461198.jpeg?dpr=2&h=480&w=640';
+const price = 10.99;
+
+const product = (
+  <div>
+    <img src={imageUrl} alt="Tacos With Lime" width="640" />
+    <h2>Tacos With Lime</h2>
+    <p>Price: {price}$</p>
+    <button type="button">Add to cart</button>
+  </div>
+);
+
+ReactDOM.render(product, document.getElementById('root'));
