3632a416703528d72ada5f458bccb4a2ba0e39d4
ludmiladev <45207186+ludmiladev@users.noreply.github.com>
2020-12-01 16:42:47 +0200

    Инлайн CSS

--- src/index.js
+++ src/index.js
@@ -17,9 +17,9 @@ console.log("создаю линку", link);
 
 ReactDOM.render(link, document.getElementById('root'));
 */
-/*
-==================V3  создает рисунок======================================
 
+// ==================V3  создает рисунок======================================
+/*
 const image = React.createElement('img', {
   src:
     'https://images.pexels.com/photos/461198/pexels-photo-461198.jpeg?dpr=2&h=480&w=640',
@@ -36,8 +36,8 @@ const productWithChildrenInProps = React.createElement('div', {
   children: [image, title, price, button],
 });
 
-ReactDOM.render(image, document.getElementById('root'));  */
-
+ReactDOM.render(image, document.getElementById('root'));  
+*/
 
 // ========================================= V4   JavaScript Syntax Extension (JSX)===========
 
@@ -119,7 +119,7 @@ ReactDOM.render(<Product />, document.getElementById('root'));
 
 
 // ================== Свойства компонента (props) ====================================================================================
-
+/*
 const Product = ({ imgUrl, name, price }) => (
   <div>
     <img src={imgUrl} alt={name} width="640" />
@@ -144,5 +144,143 @@ const App = () => (
   </div>
 );
 ReactDOM.render(<App />, document.getElementById('root'));
-
+*/
 // ====================== Свойство props.children =======================================================================================
+
+// const Qwerty = 28;
+// console.log('Qwerty :>> ', Qwerty);
+// ====================== Свойство props.children ==========================================================================
+
+/*
+const Profile = ({ name, email }) => (
+  <div>
+    <p>Name: {name}</p>
+    <p>Email: {email}</p>
+  </div>
+);
+
+const Panel = ({ title, children }) => (
+  <section>
+    <h2>{title}</h2>
+    {children}
+  </section>
+);
+
+const App = () => (
+  <div>
+    <Panel title="User profile">
+      <Profile name="Mango" email="mango@mail.com" />
+    </Panel>
+  </div>
+);
+ReactDOM.render(<Panel />, document.getElementById('root'));*/
+
+/*
+const image = React.createElement('img', {
+  src:
+    'https://images.pexels.com/photos/461198/pexels-photo-461198.jpeg?dpr=2&h=480&w=640',
+  alt: 'Tacos With Lime',
+  width: 640,
+});
+const title = React.createElement('h2', null, 'Tacos With Lime');
+const price = React.createElement('p', null, 'Price: 10.99$');
+const button = React.createElement('button', { type: 'button' }, 'Add to cart');
+
+const product = React.createElement('div', null, image, title, price, button);
+
+const productWithChildrenInProps = React.createElement('div', {
+  children: [image, title, price, button],
+});
+
+ReactDOM.render(image, document.getElementById('root'));  */
+
+
+// =========================================================================================================
+/*
+const Product = ({ imgUrl, name, price, quantity }) => (
+  <div>
+    <img src={imgUrl} alt={name} width="640" />
+    <h2>{name}</h2>
+    <p>Price: {price}$</p>
+    <h1>Quantity: {quantity < 20 ? 'Few left' : 'In stock'}</h1>
+    <button type="button">Add to cart</button>
+  </div>
+);
+ReactDOM.render(<Product />, document.getElementById('root')); */
+
+// =====================Коллекции===================================================================================
+/*
+const favouriteBooks = [
+  { id: 'id-1', name: 'JS for beginners' },
+  { id: 'id-2', name: 'React basics' },
+  { id: 'id-3', name: 'React Router overview' },
+  { id: 'id-4', name: 'Redux in depth' },
+];
+
+const BookList = ({ books }) => (
+  <ul>
+    {books.map(book => (
+      <li>{book.name}</li>
+    ))}
+  </ul>
+);
+
+ReactDOM.render(
+  <BookList books={favouriteBooks} />,
+  document.getElementById('root'),
+);
+
+*/
+// ================================================Ключи================================================================
+/*
+const favouriteBooks = [
+  { id: 'id-1', name: 'JS for beginners' },
+  { id: 'id-2', name: 'React basics' },
+  { id: 'id-3', name: 'React Router overview' },
+  { id: 'id-4', name: 'Redux in depth' },
+];
+
+const BookList = ({ books }) => (
+  <ul>
+    {books.map(book => (
+      <li key={book.id}>{book.name}</li>
+    ))}
+  </ul>
+);
+
+ReactDOM.render(
+  <BookList books={favouriteBooks} />,
+  document.getElementById('root'),
+);
+*/
+
+// =====================  Инлайн CSS================================================
+
+const buttonStyles = {
+  display: 'inline-flex',
+  margin: '0 4px',
+  padding: '8px 24px',
+  border: 0,
+  borderRadius: 2,
+  fontSize: 14,
+  fontFamily: 'inherit',
+};
+
+const Button = ({ type = 'button', label, disabled }) => (
+  <button
+    type={type}
+    disabled={disabled}
+    style={{
+      ...buttonStyles,
+      backgroundColor: disabled ? '#0000001f' : '#2196f3',
+      color: disabled ? '#00000042' : '#ffffff',
+    }}
+  >
+    {label}
+  </button>
+);
+
+ReactDOM.render(
+  <Button />,
+  document.getElementById('root'),
+);
